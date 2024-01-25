e6401cb90f82347a59911937e0700da6f2df061f
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-01-24 21:37:08 -0500

    Got basic content working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+});
+
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
