2178605fe833fefaa1204fae6df0b8a278c69b83
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-01-26 10:01:25 -0500

    Got RSS working nicely

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    plugins: ['prettier-plugin-astro'],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
-      },
-    ],
+  site: "https://www.larrycancode.com",
+  plugins: ['prettier-plugin-astro'],
+  overrides: [{
+    files: '*.astro',
+    options: {
+      parser: 'astro'
+    }
+  }],
+  integrations: [preact()]
 });
 
 // .prettierrc.mjs
