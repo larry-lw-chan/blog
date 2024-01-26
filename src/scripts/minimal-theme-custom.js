f6a4834d48909518f111bc3a895d0990b8ad6f9a
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-01-26 13:41:48 -0500

    Load global javascript after DOM loads to fix safari bug part 2

--- src/scripts/minimal-theme-custom.js
+++ src/scripts/minimal-theme-custom.js
@@ -1,21 +1,23 @@
-const button = document.querySelector("a[data-theme-switcher]");
-const light = "🔆";
-const dark = "🌙";
-
-
-// Add switching button functionality
-button.addEventListener("click", function () {
-  if (button.getAttribute("data-theme-switcher") === "light") {
-    button.setAttribute("data-theme-switcher", "dark");
-    button.textContent = light;
-  } else {
-    button.setAttribute("data-theme-switcher", "light");
-    button.textContent = dark;
-  }
+window.addEventListener("load", () => {
+  const button = document.querySelector("a[data-theme-switcher]");
+  const light = "🔆";
+  const dark = "🌙";
+  
+  // Add switching button functionality
+  button.addEventListener("click", function () {
+    if (button.getAttribute("data-theme-switcher") === "light") {
+      button.setAttribute("data-theme-switcher", "dark");
+      button.textContent = light;
+    } else {
+      button.setAttribute("data-theme-switcher", "light");
+      button.textContent = dark;
+    }
+  });
+  
+  // Set correct buttons on reload
+  const currentTheme = localStorage.getItem("picoPreferredColorScheme");
+  currentTheme == "dark"
+    ? (button.textContent = dark)
+    : (button.textContent = light);
 });
 
-// Set correct buttons on reload
-const currentTheme = localStorage.getItem("picoPreferredColorScheme");
-currentTheme == "dark"
-  ? (button.textContent = dark)
-  : (button.textContent = light);
