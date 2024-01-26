8a15c7d0257a1a84cd76a5467c4ccc05432e9534
Larry Chan <1851426+larry-lw-chan@users.noreply.github.com>
2024-01-26 11:36:00 -0500

    Added some styling to blog

---
+++ src/scripts/global.js
@@ -0,0 +1,8 @@
+  // Sets custom color for box-icons
+  const style = getComputedStyle(document.body);
+  const color = style.getPropertyValue("--color");
+  const boxIcons = document.querySelectorAll("box-icon");
+  
+  boxIcons.forEach((boxIcon) => {
+    boxIcon.setAttribute("color", color);
+  });
